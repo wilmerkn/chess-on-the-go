25fca3e6c249afa776856626f5dd49e0369e9ad6
Danny Gazic <dannygazic@gmail.com>
2022-04-12 21:46:13 +0200

    moved map initializer to controller

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -33,42 +33,6 @@ public class GameModel {
         chesspieces.put(12, new ChessPiece(ChessPieceColor.BLACK, ChessPieceType.KING)); //black King
     }
 
-    public void initializeMap(){
-        ChessPieceAbstract[][] gamemap = map.getMap();
-
-        //load black chesspieces
-        gamemap[0][0] = chesspieces.get(8);
-        gamemap[0][1] = chesspieces.get(9);
-        gamemap[0][2] = chesspieces.get(10);
-        gamemap[0][3] = chesspieces.get(11);
-        gamemap[0][4] = chesspieces.get(12);
-        gamemap[0][5] = chesspieces.get(10);
-        gamemap[0][6] = chesspieces.get(9);
-        gamemap[0][7] = chesspieces.get(8);
-
-        //loads black Pawns
-        int row = 1;
-        for(int col = 0; col < map.getMapDimension(); col++){
-            gamemap[row][col] = chesspieces.get(7);
-        }
-
-        //load white Pawns
-        row = 6;
-        for(int col = 0; col < map.getMapDimension(); col++){
-            gamemap[row][col] = chesspieces.get(1);
-        }
-
-        //load white chesspieces
-        gamemap[7][0] = chesspieces.get(2);
-        gamemap[7][1] = chesspieces.get(3);
-        gamemap[7][2] = chesspieces.get(4);
-        gamemap[7][3] = chesspieces.get(5);
-        gamemap[7][4] = chesspieces.get(6);
-        gamemap[7][5] = chesspieces.get(4);
-        gamemap[7][6] = chesspieces.get(3);
-        gamemap[7][7] = chesspieces.get(2);
-    }
-
     public Player[] getPlayers() {
         return players;
     }
