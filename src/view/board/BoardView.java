91aa00eeb3fa4f543ca135bae87e9dd1b8c379c4
qweqweqwe <kylberg@gmail.com>
2022-04-14 01:01:17 +0200

    Added mouseListener

--- src/view/board/BoardView.java
+++ src/view/board/BoardView.java
@@ -2,8 +2,13 @@ package view.board;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.awt.event.MouseEvent.BUTTON1;
 
 
 public class BoardView {
@@ -15,6 +20,7 @@ public class BoardView {
     private final static Dimension SQUARE_DIMENSION = new Dimension(15, 15);
 
     public BoardView() {
+
         this.frame = new JFrame();
         this.boardPanel = new BoardPanel();
         this.frame.setTitle("Chess");
@@ -27,17 +33,42 @@ public class BoardView {
 
     private class BoardPanel extends JPanel {
 
-        JComponent[][] buttons = new JComponent[8][8];
-        int colorCount = 0;
+        JComponent[][] squares = new JComponent[8][8];
         public BoardPanel() {
             super(new GridLayout(8, 8));
             for (int row = 0; row < 8; row++){
                 for(int col = 0; col < 8; col++){
-                    SquarePanel squarePanel = new SquarePanel();
-                    squarePanel.assignColor(colorCount);
-                    buttons[row][col] = squarePanel;
+                    SquarePanel squarePanel = new SquarePanel(row, col);
+                    squares[row][col] = squarePanel;
                     this.add(squarePanel);
-                    colorCount++;
+                    squarePanel.addMouseListener(new MouseListener() {
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+                            if(e.getButton() == BUTTON1)  {
+                                System.out.println("BUTTON1 clicked: " + "Row: " + squarePanel.getRow() + " Col: " + squarePanel.getCol());
+                            }
+                        }
+
+                        @Override
+                        public void mousePressed(MouseEvent e) {
+
+                        }
+
+                        @Override
+                        public void mouseReleased(MouseEvent e) {
+
+                        }
+
+                        @Override
+                        public void mouseEntered(MouseEvent e) {
+
+                        }
+
+                        @Override
+                        public void mouseExited(MouseEvent e) {
+
+                        }
+                    });
                 }
             }
 
@@ -49,15 +80,27 @@ public class BoardView {
 
     private class SquarePanel extends JPanel {
 
-        public SquarePanel () {
+        private final int row;
+        private final int col;
 
+        public SquarePanel (int row, int col) {
+            this.row = row;
+            this.col = col;
+            assignColor();
             setPreferredSize(SQUARE_DIMENSION);
             setVisible(true);
         }
 
-        public void assignColor(int squareID) {
-            int row = squareID/8;
-            if(squareID % 2 == 0 ) {
+        public int getRow() {
+            return row;
+        }
+
+        public int getCol() {
+            return col;
+        }
+
+        public void assignColor() {
+            if(col % 2 == 0 ) {
                 if(row % 2 == 0) {
                     setBackground(Color.CYAN);
                 } else {
