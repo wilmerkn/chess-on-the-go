6bab11c4f4378f6f8bede6d84a468e0ffaa21c19
wilmerkn <wilmer.k@icloud.com>
2022-04-20 14:24:01 +0200

    kommentar

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,6 +5,7 @@ import server.controller.LoginController;
 public class Client {
 
     public static void main(String[] args) {
+        //testkör här för att komma in via login etc /wilmer
         LoginController loginController = new LoginController();
 
 
