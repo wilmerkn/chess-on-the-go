19fe6ffa2b49bc251dbd788ea8015a7aec1e2135
wilmerkn <wilmer.k@icloud.com>
2022-04-22 09:50:03 +0200

    kommentar till testare

--- src/client/Client.java
+++ src/client/Client.java
@@ -6,6 +6,7 @@ public class Client {
 
     public static void main(String[] args) {
         //testkör här för att komma in via login etc /wilmer
+        //För er som testar, är lite osäker på om det kommer funka med databasconnection i och med att det är localhost för mig
         LoginController loginController = new LoginController();
 
 
