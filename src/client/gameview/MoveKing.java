e3e52d5cbd8307170542691529ba4b9a44031597
mirsmi <89800037+mirsmi@users.noreply.github.com>
2022-06-01 21:32:37 +0200

    worked on chess rules

---
+++ src/client/gameview/MoveKing.java
@@ -0,0 +1,20 @@
+package client.gameview;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MoveKing implements ActionListener{
+    private JFrame frame;
+
+    public MoveKing(){
+        frame = new JFrame("Check!");
+        JOptionPane.showMessageDialog(frame,"King is in check! Move the king or eliminate the enemy!");
+    }
+
+    //action performed method
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        frame.dispose();
+    }
+}
+
